5f16262fc7dd0e5883237d365cacb169b9b415d6
Makhuta <matykubla@gmail.com>
2025-07-29 19:23:18 +0200

    Update
    - separated workflow build for GHRC and Docker Hub into separate files

---
+++ entrypoint_dockerhub.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+set -e
+
+# Now your Python/Django and other setup
+
+if [ ! -d "/media/static" ]; then
+    mkdir -p /media/static
+fi
+
+cp -r /media_static_temp/* /media/static/ 2>/dev/null || true
+
+python manage.py makemigrations api database discord discordoauth2 frontend script
+python manage.py migrate
+python manage.py makemigrations api database discord discordoauth2 frontend script
+python manage.py migrate
+python manage.py collectstatic --noinput
+python manage.py make-messages -a --ignore=project_root/*
+django-admin compilemessages
+
+gunicorn --bind 0.0.0.0:8000 core.wsgi:application & nginx -g 'daemon off;'
