9c35f44d9efdf3df0a0e31af3988ff666d51cfb1
Makhuta <matykubla@gmail.com>
2025-09-15 18:34:11 +0200

    Fix
    - fix issue that "Account is already in use"

--- dabing-manager/core/settingz/discord_auth.py
+++ dabing-manager/core/settingz/discord_auth.py
@@ -15,6 +15,7 @@ SOCIAL_AUTH_PIPELINE = (
     'social_core.pipeline.social_auth.auth_allowed',
     'discordoauth2.pipeline.check_discord_whitelist',
     'discordoauth2.pipeline.create_user_if_not_exists',
+    'discordoauth2.pipeline.associate_discord_user',
     'social_core.pipeline.social_auth.associate_user',
     'social_core.pipeline.social_auth.load_extra_data',
     'social_core.pipeline.user.user_details',
--- dabing-manager/discordoauth2/pipeline.py
+++ dabing-manager/discordoauth2/pipeline.py
@@ -1,20 +1,33 @@
-from social_core.exceptions import AuthForbidden
-from discord.models import DiscordUser
-from django.contrib.auth import get_user_model
-from .exceptions import DiscordLoginNotAllowed
 from django.shortcuts import redirect
 from django.urls import reverse
+from django.contrib.auth import get_user_model
+from social_django.models import UserSocialAuth
+from social_core.exceptions import AuthForbidden
+from discord.models import DiscordUser
+
 User = get_user_model()
 
 def check_discord_whitelist(strategy, details, backend, uid, *args, **kwargs):
+    """
+    Only allow login if Discord user is whitelisted (is_member=True)
+    """
     if backend.name != 'discord':
         return
     d_user = DiscordUser.objects.filter(discord_id=uid, is_member=True)
     if not d_user.exists():
+        # Redirect to 'not_allowed' page if not whitelisted
         return redirect(reverse("not_allowed"))
 
 
 def create_user_if_not_exists(strategy, details, backend, uid, user=None, *args, **kwargs):
+    """
+    Create a Django user if it does not exist.
+    If the DiscordUser is already linked to a user, return that user.
+    """
+    if backend.name != 'discord':
+        return
+
+    # If a user is already passed (logged in), just return it
     if user:
         return {'user': user}
 
@@ -23,8 +36,12 @@ def create_user_if_not_exists(strategy, details, backend, uid, user=None, *args,
     if discord_user_qs is None:
         return redirect(reverse("not_allowed"))
 
-    discord_discriminator = details.get('username') or details.get('discriminator')
+    # If this DiscordUser is already linked to a Django user, use that
+    if discord_user_qs.user:
+        return {'user': discord_user_qs.user}
 
+    # Create a new Django user
+    discord_discriminator = details.get('username') or details.get('discriminator')
     if discord_discriminator:
         django_username = f"{discord_discriminator}_{discord_id}"
     else:
@@ -32,8 +49,27 @@ def create_user_if_not_exists(strategy, details, backend, uid, user=None, *args,
 
     user, created = User.objects.get_or_create(username=django_username)
 
-    if created or discord_user_qs.user is None:
-        discord_user_qs.user = user
-        discord_user_qs.save()
+    # Link DiscordUser to this Django user
+    discord_user_qs.user = user
+    discord_user_qs.save()
 
     return {'user': user}
+
+
+def associate_discord_user(strategy, uid, user=None, backend=None, *args, **kwargs):
+    """
+    Associate the Discord account with a user.
+    If the Discord account is already linked to another user, use that user.
+    """
+    if not backend or backend.name != 'discord':
+        return
+
+    # Check if this Discord account is already linked
+    social_qs = UserSocialAuth.objects.filter(provider='discord', uid=uid)
+    if social_qs.exists():
+        return {'user': social_qs.first().user}
+
+    # If not linked, associate with current user
+    if user:
+        UserSocialAuth.objects.create(user=user, provider='discord', uid=uid)
+        return {'user': user}
